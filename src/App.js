19524d47e6fd691be282609b4cfdf68be2316a5c
Anubhav verma <anubhavav04@gmail.com>
2024-07-31 01:22:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
 
   return (
     <div className='container'>
-      <img src={url} className='img' />
+      <img src={url} className='img' alt='Image' />
       <div className='wrapper'>
         <h1>Simple file sharing!</h1>
         <p>Upload and share the download link.</p>
