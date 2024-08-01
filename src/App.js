56591959bef6d7a0038b89e7c9ed823d397dc0af
Anubhav verma <anubhavav04@gmail.com>
2024-08-01 10:39:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -45,7 +45,7 @@ function App() {
           style={{ display: "none" }}
           onChange={(e) => setFile(e.target.files[0])}
         />
-        {resultavailable && ( <a href={result} target="_blank" rel="noreferrer">Click here to download the file !!</a>)}
+        {resultavailable && ( <a href={result} target="_blank" rel="noreferrer">{result}</a>)}
        
       </div>
     </div>
