be046fa7b2bcd79adcf5686461c6f508d0603251
Anubhav verma <anubhavav04@gmail.com>
2024-08-01 10:50:25 +0530

    Update api.js

--- src/service/api.js
+++ src/service/api.js
@@ -1,3 +1,15 @@
+// import axios from 'axios';
+
+// const API_URI = 'https://file-sharing-app-backend-9kme.onrender.com';
+
+// export const uploadFile = async (data) => {
+//     try {
+//         const response = await axios.post(`${API_URI}/upload`, data);
+//         return response.data;
+//     } catch (error) {
+//         console.log('Error while calling the API ', error.message);
+//     }
+// }
 import axios from 'axios';
 
 const API_URI = 'https://file-sharing-app-backend-9kme.onrender.com';
@@ -7,6 +19,7 @@ export const uploadFile = async (data) => {
         const response = await axios.post(`${API_URI}/upload`, data);
         return response.data;
     } catch (error) {
-        console.log('Error while calling the API ', error.message);
+        console.log('Error while calling the API:', error.message);
+        throw error; // Throw error to let the calling function handle it
     }
 }
